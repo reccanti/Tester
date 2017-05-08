4e39f84347a891584617ebadbcb1396f4b734605
Ben Wilcox <reccanti@gmail.com>
2017-05-08 00:22:10 -0400

    Initial Commit

---
+++ Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tester
+//
+//  Created by Benjamin Wilcox on 5/8/17.
+//  Copyright © 2017 Benjamin Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
